0911bb6f324b2321fd79965ccca312d71f338773
Abel214 <131829158+Abel214@users.noreply.github.com>
2023-05-18 21:30:31 -0500

    Clase noticias

--- index.java
+++ index.java
@@ -1,1 +1,8 @@
-Prueba 
+public class New {
+    public static void main(String[] args) {
+        System.out.println(" Informar resultado");
+        System.out.println(" Informar sobre el team");
+        System.out.println(" Informar sobre entrevistas");
+        System.out.println(" Informar sobre jugador");
+
+}
